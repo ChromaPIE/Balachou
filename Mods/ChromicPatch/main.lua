7b742afa1d26891a55ec2908dc12fe4be6db8158
Pierre Zhang <chromasetha@outlook.com>
2026-05-18 00:08:54 +0800

    upd patch

---
+++ Mods/ChromicPatch/main.lua
@@ -0,0 +1,10 @@
+SMODS.Font {
+	key = "xuandong",
+	path = "MaShanZheng-Regular.ttf",
+	render_scale = 400,
+	TEXT_HEIGHT_SCALE = 1.0,
+	TEXT_OFFSET = { x = 0, y = 0 },
+	FONTSCALE = 0.05,
+	squish = 1,
+	DESCSCALE = 1
+}
